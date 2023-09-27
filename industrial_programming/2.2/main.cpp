6e6f4a5788fd3a5e16ee478118a296db60a2f2de
SvPg <62066211+oreg0na@users.noreply.github.com>
2023-09-27 09:39:45 +0300

    fix 2.2

--- industrial_programming/2.2/main.cpp
+++ industrial_programming/2.2/main.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while (a != b){
+        if (a % 2 == 0 and a / 2 >= b){
+            a = a/2;
+            cout <<":2"<<endl;
+        } else {
+            a = a - 1;
+            cout <<"-1"<<endl;
+        }
+    }
+    return 0;
+}
