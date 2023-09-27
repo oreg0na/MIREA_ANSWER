e240502f8e36f658f01ca4c17dbf6a85a26a1adb
SvPg <62066211+oreg0na@users.noreply.github.com>
2023-09-27 09:38:06 +0300

    fix

--- industrial_programming/1.3/main.cpp
+++ industrial_programming/1.3/main.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a;
+	float b;
+	string c;
+	cin >> a >> c >> b;
+	if(c == "+"){
+	    cout << a + b;
+	}
+	if(c == "-"){
+	    cout << a - b;
+	}
+	if(c == "*"){
+	    cout << a * b;
+	}
+	if(c == "/"){
+	    cout << a / b;
+	}
+	return 0;
+}
