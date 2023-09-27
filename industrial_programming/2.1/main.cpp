657c3d982e5d72e5aca436379f890ae19240ec31
SvPg <62066211+oreg0na@users.noreply.github.com>
2023-09-27 09:39:05 +0300

    fix 2.1

--- industrial_programming/2.1/main.cpp
+++ industrial_programming/2.1/main.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    long double x, y, poc;
+    poc = 10;
+    int day = 1;
+    cin >> x >> y >> poc;
+    while (x < y){
+        x = x + (x / 100) * poc;
+        day++;
+    }
+    cout << day;
+    return 0;
+}
