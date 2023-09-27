c0a1d0f110b569268e31f9b96f32fed7b7767888
SvPg <62066211+oreg0na@users.noreply.github.com>
2023-09-27 09:36:55 +0300

    fix main.cpp

--- industrial_programming/1.2/main.cpp
+++ industrial_programming/1.2/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    double a;
+    double b;
+    cin >> a >> b;
+    cout << a + b<<" "<<a - b<<" "<< a * b<<" "<<a / b;
+    return 0;
+}
