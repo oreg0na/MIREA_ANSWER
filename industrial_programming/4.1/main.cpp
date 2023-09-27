ad4459de0c98d45fac706fb55578e2e8442c42fc
SvPg <62066211+oreg0na@users.noreply.github.com>
2023-09-27 09:41:57 +0300

    fix 4.1

--- industrial_programming/4.1/main.cpp
+++ industrial_programming/4.1/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
 
+int main() {
+	std::string x, y, y_rev;
+	std::cin >> x >> y;
+	y_rev = y;
+	std::reverse(y_rev.begin(), y_rev.end());
+	if (x.find(y) != std::string::npos || x.find(y_rev) != std::string::npos) {
+		std::cout << "yes";
+	}
+	else {
+		std::cout << "no";
+	}
+}
