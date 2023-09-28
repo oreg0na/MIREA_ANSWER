9a7a348b81dcaab8f1125c4bc713cba88d6a37fa
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2023-09-28 12:54:19 +0300

    Update main.cpp

--- algorithms/2.2/1/main.cpp
+++ algorithms/2.2/1/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
-template <class _Type>
-bool is_pow(_Type _number) {
-    double number_buffer = std::sqrt(_number);
-    return static_cast<_Type>(number_buffer) == number_buffer ? true : false;
-}
-
 int main() {
-    int64_t number = 0;
-    std::cin >> number;
-    std::cout << (is_pow(number) ? "true" : "false");
+	double number = 0.0;
+	std::cin >> number;
+	double index = 0.0;
+	while (1) {
+		double number_buffer = std::pow(2, index);
+		if (number_buffer == number) {
+			std::cout << 1;
+			break;
+		} else if (number_buffer > number) {
+			std::cout << 0;
+			break;
+		}
+		index += 1.0;
+	}
+
 }
