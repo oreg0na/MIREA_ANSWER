b514a55198b29145b499de5461e5b070cd72338e
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2023-09-28 14:00:40 +0300

    Update main.cpp

--- algorithms/2.2/3/main.cpp
+++ algorithms/2.2/3/main.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
 
-template <class _Type>
-bool is_simple(_Type _number) {
-	for (auto i = 2ull; i < static_cast<decltype(i)>(std::sqrt(_number)) + 1; i++) {
-		if (_number % i == 0) return false;
+template <class _VectorType, class _NumberType>
+std::vector<_VectorType> get_simple_devinders(_NumberType _number) {
+	std::vector<_VectorType> devinders = {};
+	_NumberType number_buffer = _number;
+	while (number_buffer != 1) {
+		if (number_buffer % 5 == 0) {
+			devinders.push_back(5);
+			number_buffer /= 5;
+		} else if (number_buffer % 3 == 0) {
+			devinders.push_back(3);
+			number_buffer /= 3;
+		} else if (number_buffer % 2 == 0) {
+			devinders.push_back(2);
+			number_buffer /= 2;
+		} else return std::vector<_VectorType>();
 	}
-	return true;
-}
-
-template <class _Type>
-bool is_special_number(_Type _number) {
-	bool is_2_devinder = false, is_3_devinder = false, is_5_devinder = false;
-	for (auto i = 2ull; i < _number; i++) {
-		if (_number % i == 0) {
-			if (i == 2) is_2_devinder = true;
-			else if (i == 3) is_3_devinder = true;
-			else if (i == 5) is_5_devinder = true;
-			else {
-				if (is_simple(i)) return false;
-			}
-		}
-	}
-
-	if (is_2_devinder && is_3_devinder && is_5_devinder) return true;
-	return false;
+	return devinders;
 }
 
 int main() {
 	size_t index = 0;
 	std::cin >> index;
-	size_t finded_numbers = 0;
-	int64_t number = 30;
-	while (1) {
-		if (is_special_number(number)) {
-			finded_numbers += 1;
-			if (finded_numbers == index) break;
+	if (index == 1) std::cout << 1;
+	else {
+		size_t number = 2;
+		size_t last_index = 2;
+		while (1) {
+			if (last_index == index) {
+				std::cout << number;
+				break;
+			}
+
+			number += 1;
+			std::vector<size_t> devinders = get_simple_devinders<size_t>(number);
+			if (devinders.size() > 0) last_index += 1;
 		}
-		number += 1;
 	}
-	std::cout << number;
 }
