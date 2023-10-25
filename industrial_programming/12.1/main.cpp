ca0c3be80c17ca918a364ded822795c05c652f8d
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-25 21:57:22 +0300

    Create main.cpp

---
+++ industrial_programming/12.1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+
+template <class number_type>
+void reverse_numbers(number_type _number, size_t _count) {
+	if (!_count) return;
+	number_type number = 0;
+	std::cin >> number;
+	reverse_numbers(number, _count - 1);
+	std::cout << number << ' ';
+}
+
+int main() {
+	size_t count = 0;
+	std::cin >> count;
+
+	uint64_t number = 0;
+	std::cin >> number;
+	reverse_numbers(number, count - 1);
+	std::cout << number;
+}
