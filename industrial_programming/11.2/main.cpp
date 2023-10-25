cd1547c9359a4f687f6699fa093d8ad553701bb4
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-25 09:57:54 +0300

    Create main.cpp

---
+++ industrial_programming/11.2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <class number_type>
+number_type gsd(number_type _firstNumber, number_type _secondNumber) {
+    if(_secondNumber == 0) return _firstNumber;
+    if(_firstNumber > _secondNumber) return gsd(_secondNumber, _firstNumber % _secondNumber);
+    else return gsd(_firstNumber, _secondNumber % _firstNumber);
+}
+
+int main(){
+    size_t a = 0, b = 0;
+    cin >> a >> b;
+    cout << gsd(a, b);
+}
