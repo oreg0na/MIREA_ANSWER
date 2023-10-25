00c64ff88fd71d352f3fbaf0c753e2c87833c392
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-25 09:52:48 +0300

    Create main.cpp

---
+++ industrial_programming/10.2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+#include <ranges>
+#include <algorithm>
+
+int main() {
+    size_t count = 0;
+    double summ = 0.0;
+    std::cin >> count;
+    std::random_device device;
+    std::mt19937 generate(device());
+    double* num = new double[count];
+    for (auto i = 0ull; i < count; i++) {
+        num[i] = static_cast<double>(i+1);
+        summ += num[i];
+    }
+    std::ranges::shuffle(&num[0], &num[count-1], generate);
+    std::cout << summ;
+    delete[] num;
+}
