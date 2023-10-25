71b0f21556f22cc0f3dbc084cda06a0bda281d72
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-25 22:02:15 +0300

    Create main.cpp

---
+++ industrial_programming/13.2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+wchar_t to_lower(wchar_t _symbol) {
+	if (_symbol >= L'А' && _symbol <= L'Я') return _symbol + (L'а' - L'А');
+	else if (_symbol >= L'A' && _symbol <= L'Z') return _symbol + (L'a' - L'A');
+	else return _symbol;
+}
+
+int main() {
+	std::wcin.imbue(std::locale("ru_RU.cp1251"));
+	std::wcout.imbue(std::locale("ru_RU.cp1251"));
+	std::wstring text;
+	std::getline(std::wcin, text);
+	for (auto& symbol : text) {
+		symbol = to_lower(symbol);
+	}
+	std::wcout << text;
+}
