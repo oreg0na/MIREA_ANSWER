e288da65b7135db68b369f0808cbcc0831228bfb
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-25 09:54:52 +0300

    Create main.cpp

---
+++ industrial_programming/10.3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int64_t a = 0, b = 0, c = 0;
+    cin >> a >> b >> c;
+    int64_t first_n = a;
+    int64_t second_n = b;
+    for (auto i = 2; i < c; i++){
+        int64_t n_buffer = first_n * second_n;
+        first_n = second_n,second_n = n_buffer;
+    }
+    cout << second_n << endl;
+}
