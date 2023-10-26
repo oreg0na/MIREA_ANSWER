4821c628985810d2d0f03c9161993811cfa23206
oreg0na <62066211+oreg0na@users.noreply.github.com>
2023-10-26 17:53:08 +0300

    Update main.cpp

--- industrial_programming/13.2/main.cpp
+++ industrial_programming/13.2/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
+#include <cctype>
 #include <string>
 
-wchar_t to_lower(wchar_t _symbol) {
-	if (_symbol >= L'А' && _symbol <= L'Я') return _symbol + (L'а' - L'А');
-	else if (_symbol >= L'A' && _symbol <= L'Z') return _symbol + (L'a' - L'A');
-	else return _symbol;
+int main() {
+    std::string text;
+    std::getline(std::cin, text);
+
+    for (char & c : text)
+        c = std::tolower(c);
+
+    std::cout << text << std::endl;
+
+    return 0;
 }
 
-int main() {
-	std::wcin.imbue(std::locale("ru_RU.cp1251"));
-	std::wcout.imbue(std::locale("ru_RU.cp1251"));
-	std::wstring text;
-	std::getline(std::wcin, text);
-	for (auto& symbol : text) {
-		symbol = to_lower(symbol);
-	}
-	std::wcout << text;
-}
